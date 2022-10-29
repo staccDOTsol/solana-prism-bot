22e27ae1d76245eaac8f3cca53e4bb90c6cf6556
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-29 16:14:52 -0300

    program

--- getlutsreal.mjs
+++ getlutsreal.mjs
@@ -18074,6 +18074,30 @@ const createWSolAccount = async (mint) => {
 let prev = new Date().getTime() / 1000;
 let avgs = [];
 async function something(SOL_MINT, myluts) {
+
+async function ha (){
+
+  var afile = JSON.parse( fs.readFileSync('./answersusdc.json').toString() )
+  let c = -1
+  var newobj = {}
+  for (var lutlist of Object.values(afile)){
+      c++
+      let c2 = -1
+  for (var addy of Object.keys(afile)[c].split(',')){
+  c2++ 
+  if (c2 > 1){
+      if (!Object.keys(newobj).includes(addy)){
+      newobj[addy] = lutlist
+      
+  }
+  newobj[addy].push(...lutlist)
+  }
+  }
+  }
+  fs.writeFileSync('./newanswers.json', JSON.stringify(newobj))
+  }
+  
+  ha () 
   let jares = [];
 
   SOL_MINT = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"// mints[rando(0, mints.length)];
