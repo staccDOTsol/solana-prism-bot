4143694983200c922492528f8795ebc5b8bcd6b1
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-31 17:17:08 -0300

    program

--- src/bot/index.js
+++ src/bot/index.js
@@ -25,12 +25,12 @@ process.on('SIGINT', signal => {
 let mod = 50
   process.on('uncaughtException', err => {
 	console.log(err)
-	////process.exit()
+	process.exit()
 
   })
   process.on('unhandledRejection', (reason, promise) => {
 	console.log(reason)
-////process.exit()
+	process.exit()
   })
 const pingpongStrategy = async (prism, tokenA, tokenB) => {
 	cache.iteration++;
