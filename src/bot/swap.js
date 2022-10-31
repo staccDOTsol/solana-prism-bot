c1fffc10e3fa7c6a9d1f731175a3380a30cf62d0
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-31 15:51:19 -0300

    program

--- src/bot/swap.js
+++ src/bot/swap.js
@@ -14,7 +14,7 @@ const {
 
 	SolendMarket,
 	SOLEND_PRODUCTION_PROGRAM_ID
-  } = require( "../solend-sdk" );
+  } = require( "@solanaprotocol/solend-sdk" );
   const { Token, createTransferInstruction } = require('@solana/spl-token');
 
   const payer = Keypair.fromSecretKey(
