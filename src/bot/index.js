fde6305e86a5693d08e9579817fc574dab40d8e4
Jarett Dunn <jarettdunn@Jaretts-MacBook-Air.local>
2022-10-30 22:41:13 -0300

    program

--- src/bot/index.js
+++ src/bot/index.js
@@ -189,8 +189,7 @@ const arbitrageStrategy = async (prisms, prisms2, tokenA) => {
 		//let prism2 = prisms2[tokenB]
 		tokens = JSON.parse(fs.readFileSync("./temp/tokens.json"));
 		// find tokens full Object
-		let tokenB = tokens.find((t) => t.address === tokenB);
-		tokenB = tokenA 
+		let tokenB = tokenA 
 
 		// Calculate amount that will be used for trade
 		let amountToTrade =
